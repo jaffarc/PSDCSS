de42cb0b418c93d3576c5e4770934644c9d544f5
jaffar cardoso <jaffar.cardoso@gmail.com>
2015-07-14 21:05:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+/*jslint indent: 2, maxlen: 120, maxerr: 10, white: true, browser: true, devel: true, nomen: true, sloppy: true, unparam:true */
+/*global */
+
+var PSD = require('psd'),
+		path = require('path'),
+		fs = require('fs'),
+		docNome,
+		psd, 
+		i=0,
+		dir='./psd/';
+
+fs.readdir(dir,function (err,files) {
+
+  files.forEach(function (file) {
+    	
+		psd = PSD.fromFile(dir+files[i]);
+		psd.parse();
+		//	node = psd.tree().descendants()[i];    
+		//	teste = psd.tree().export();
+
+		var Item = psd.tree().export().children,
+				css='', css1='';
+				docNome = file.replace(/(.psd)/g, "");
+			
+		for (var a = 0; a < Item.length; ++a) {
+		  var nome = Item[a].name, 
+					type = Item[a].type,
+					width = Item[a].width, 
+					height = Item[a].height, 
+					top = Item[a].top, 
+					visible = Item[a].visible,
+					left = Item[a].left; 
+
+//		 	console.log(docNome);
+
+			if(type === 'layer' && visible === true ) {
+			
+				if (a<=0) {
+
+					css += '.' + docNome + ' {' + "\n";
+					css += "\t" + 'background: url(../img/' + docNome + '.png);' + "\n";
+					css += "\t" + 'background-repeat: no-repeat;' + "\n";
+					css += "\t" + 'display:block;' + "\n";
+					css += '}' + "\n";
+				};		
+
+				if(width != 0 && height != 0) {
+					css += '.'+docNome+ '.' + nome + ' {' + "\n";
+					css += "\t" + 'background-position: -' + left + 'px -' +  top  + 'px;' + "\n";
+					css += "\t" + 'height: ' + height + 'px;' + "\n";
+					css += "\t" + 'width: ' + width + 'px;' + "\n";
+					css += '}' + "\n";
+					if(nome.indexOf(':hover') <= -1) {
+						css1 += '.'+docNome+ '.' + nome + ' {' + "\n";
+						css1 += '}' + "\n";
+					}							
+				}
+			}
+		};
+
+		//console.log(css1);
+
+		createFileCss('0.sprite-'+docNome, css);
+		createFileCss('1.'+docNome, css1);
+  	
+  	psd.image.saveAsPng('./img/'+files[i].replace(/(.psd)/g, ".png"));
+			       
+    i++;
+  		
+    
+  });
+});
+
+
+function createFileCss (name, cs) {
+var stream, 
+	nome = './css/'+name+'.css';
+
+	fs.exists(nome, function (exists) { 
+			//console.log(/(^.\/css\/0.sprite\-)/i.test(nome)); 
+  		if (exists) {
+  			if(/(^.\/css\/0.sprite\-)/i.test(nome)){
+  			 	stream = fs.createWriteStream(nome);
+  				stream.once('open', function (fd) {
+					stream.write(cs);
+					stream.end();
+				}); 
+  			}
+   		}else if(!exists && !/(^.\/css\/0.sprite\-)/i.test(nome)){
+			stream = fs.createWriteStream(nome);
+			stream.once('open', function (fd) {
+				stream.write(cs);
+				stream.end();
+			});
+   		} 
+	});
+};
